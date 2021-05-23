62d83524dfa32941748c0899bd06f647db9ee0f3
Tomas Lin <thekingofthemountain@gmail.com>
2021-05-23 18:47:20 +0800

    add database setup

--- lastest/setup.sh
+++ lastest/setup.sh
@@ -2,6 +2,11 @@
 
 echo "copy template..."
 cp -a ./template/** ./
+echo "please update .env file"
 cp -a ./template/.env.example ./.env
 
-echo "please update .env file"
+echo "setup database"
+npx sequelize-cli db:migrate
+
+echo "create seeders"
+npx sequelize-cli db:seed:all
