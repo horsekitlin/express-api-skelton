446d84800621f85bcda01df1af8b0b4c92c97312
Tomas Lin <thekingofthemountain@gmail.com>
2021-09-12 00:01:06 +0800

    update model

--- template/database/models/user.js
+++ template/database/models/user.js
@@ -1,21 +1,52 @@
+const baseMigration = require("../config/baseMigration");
+const { saltHashPassword } = require("../../helpers/utils");
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-    // User.hasMany(models.Post, {
-    //   foreignKey: 'userId',
-    //   as: 'posts',
-    //   onDelete: 'CASCADE',
-    // });
-    
-    // User.hasMany(models.Comment, {
-    //   foreignKey: 'userId',
-    //   as: 'comments',
+  const User = sequelize.define(
+    "User",
+    {
+      name: {
+        field: "name",
+        type: DataTypes.STRING,
+        length: 20,
+      },
+      phone: {
+        field: "phone",
+        type: DataTypes.STRING,
+        length: 20,
+      },
+      email: {
+        field: "email",
+        type: DataTypes.STRING,
+        length: 50,
+        unique: true,
+      },
+      password: {
+        field: "password",
+        type: DataTypes.STRING,
+        length: 200,
+        set(value) {
+          this.setDataValue("password", saltHashPassword(value));
+        },
+      },
+    }, {
+    sequelize,
+    tableName: "users",
+    timestamps: false,
+    underscored: true,
+    freezeTableName: true,
+  });
+
+  User.associate = function (models) {
+    // User.hasMany(models.Contract, {
+    //   as: 'contracts',
+    //   foreignKey: {
+    //     name: 'user_id'
+    //   },
+    //   onUpdate: 'CASCADE',
     //   onDelete: 'CASCADE',
     // });
   };
+
   return User;
 };
